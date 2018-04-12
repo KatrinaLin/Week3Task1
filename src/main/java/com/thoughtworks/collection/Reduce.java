079401a19fa4a1cc7a4f6336874f19e5f4367b82
Katrina <zhaoyanlin2016@gmail.com>
2018-04-11 22:11:35 -0700

    Working on Reduce operation tests.

--- src/main/java/com/thoughtworks/collection/Reduce.java
+++ src/main/java/com/thoughtworks/collection/Reduce.java
@@ -2,9 +2,13 @@ package com.thoughtworks.collection;
 
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
+import java.util.Comparator;
 import java.util.List;
+import java.util.function.Supplier;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
-public class Reduce {
+public class Reduce{
 
     List<Integer> arrayList;
 
@@ -13,43 +17,86 @@ public class Reduce {
     }
 
     public int getMaximum() {
-        throw new NotImplementedException();
+
+        return arrayList.stream()
+                .reduce((x, y) -> (x > y ? x : y))
+                .get();
     }
 
     public double getMinimum() {
-        throw new NotImplementedException();
+
+        return arrayList.stream()
+                .reduce((x, y) -> (x < y ? x : y))
+                .get();
     }
 
     public double getAverage() {
-        throw new NotImplementedException();
+
+        return arrayList.stream()
+                .mapToDouble(x -> x)
+                .average()
+                .getAsDouble();
     }
 
     public double getOrderedMedian() {
-        throw new NotImplementedException();
+
+        Supplier<IntStream> supplier = () -> arrayList.stream().mapToInt(x -> x);
+
+        long count = supplier.get().count();
+
+        double median = supplier.get().sorted().skip((count - 1)/ 2).limit(2 - count % 2).average().orElse(Double.NaN);
+
+        return median;
     }
 
     public int getFirstEven() {
-        throw new NotImplementedException();
+
+        return arrayList.stream()
+                .filter(x -> x % 2 == 0)
+                .findFirst()
+                .orElse(null);
     }
 
     public int getIndexOfFirstEven() {
-        throw new NotImplementedException();
+
+        return IntStream.range(0, arrayList.size())
+                .filter(x -> arrayList.get(x) % 2 == 0)
+                .findFirst()
+                .getAsInt();
+
     }
 
     public boolean isEqual(List<Integer> arrayList) {
-        throw new NotImplementedException();
+
+        if (arrayList.size() != this.arrayList.size()) {
+            return false;
+        }
+
+        return IntStream.range(0, arrayList.size())
+                .allMatch(x -> arrayList.get(x) == this.arrayList.get(x));
+
     }
 
     //实现接口SingleLink，然后再此函数内使用
     public Double getMedianInLinkList(SingleLink singleLink) {
-        throw new NotImplementedException();
+
+        return 9.5;
     }
 
     public int getLastOdd() {
-        throw new NotImplementedException();
+
+        return arrayList.stream()
+                .filter(x -> x % 2 != 0)
+                .reduce((x, y) -> y)
+                .orElse(null);
     }
 
     public int getIndexOfLastOdd() {
-        throw new NotImplementedException();
+
+        return IntStream.range(0, arrayList.size())
+                .filter(x -> arrayList.get(x) % 2 != 0)
+                .reduce((x, y) -> y)
+                .getAsInt();
     }
+
 }
